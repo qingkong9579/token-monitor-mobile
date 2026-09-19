b195fe79e2a6e79acd02aaafc4aaad35b1e2f293
微茫之心 <2597924209@qq.com>
2026-09-19 12:03:48 +0800

    chore(release): v0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tokenmonitor.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
     }
 
     signingConfigs {
